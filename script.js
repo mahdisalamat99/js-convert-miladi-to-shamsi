3bf18766f572ba6cd20f751bf0e5c4ba51a6ef55
mahdi salamat <fannimh36@gmail.com>
2021-10-11 13:28:08 -0700

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+function shamsiDate(year,month,day){
+    const normalMonthArr = [31,28,31,30,31,30,31,31,30,31,30,31];
+    const kabiseMonthArr = [31,29,31,30,31,30,31,31,30,31,30,31];
+     let totalDay = 0; 
+     let sMonth = 0;
+     let sDay = 0;
+     let sYear = 0;
+     for (let i = 0 ; i < month  ; i++ ){
+         totalDay += kabiseDetector(year) ? kabiseMonthArr[i] : normalMonthArr[i];
+     }
+
+     
+
+     totalDay += day;
+
+     if (totalDay >79){
+         totalDay -= 79;
+         if(totalDay <= 186){
+             if(totalDay % 31 ===0){
+                 sMonth = totalDay / 31;
+                 sDay = 31;
+             } else {
+                 sMonth = Math.floor(totalDay / 31) +1 ;
+                 sDay = totalDay %31;
+             }
+         }else {
+             totalDay -= 186;
+             if (totalDay %30 ===0){
+                 sMonth = totalDay / 30+6;
+                 sDay = 30;
+             } else {
+                 sMonth = Math.floor(totalDay / 30) +7;
+                 sDay = totalDay %30 ;
+             }
+         }
+         sYear = year - 621;
+     } else {
+         let differ = kabiseDetector(year -1) ? 11 :10;
+         totalDay += differ;
+         if ( totalDay %30 === 0){
+             sMonth = totalDay / 30 + 9;
+             sDay = 30 ;
+         } else {
+             sMonth = Math.floor(totalDay / 30) + 10;
+             sDay = totalDay % 30 ;
+         }
+         sYear = year -622
+     }
+     return[sYear, sMonth,sDay]
+}
 
 
 function  kabiseDetector (input) {
@@ -7,77 +57,31 @@ function  kabiseDetector (input) {
     ){
         return true;
     } else {
-        return false
+        return false;
     }
 }
 
-function jDate(year,month,day){
-    const monthArr = [31,28,31,30,31,30,31,31,30,31,30,31];
-    const monthArrK = [31,29,31,30,31,30,31,31,30,31,30,31];
-     let totalDay = 0,
-     jMonth,
-     jDay,
-     jYear,
-     for (let i = 0 ; i < month -1 ; i++ ){
-         totalDay += kabiseDetector(year) ? monthArrK[i] : monthArr[i];
-     }
-
-     totalDay += day;
-
-     if (totalDay >79){
-         totalDay -= 79;
-         if(totalDay <= 186){
-             if(totalDay % 31 ===0){
-                 jMonth = totalDay / 31;
-                 jDay = 31;
-             } else {
-                 jMonth = Math.floor(totalDay / 31) +1 ;
-                 jDay = totalDay %31;
-                 console.log("here");
-             }
-         }else {
-             totalDay -= 186;
-             if (totalDay %30 ===0){
-                 jMonth = totalDay / 30+6;
-                 jDay = 30;
-             } else {
-                 jMonth = Math.floor(totalDay / 30) +7;
-                 jDay = totalDay %30 ;
-             }
-         }
-         jYear = year - 621;
-     } else {
-         let differ = kabiseDetector(year -1) ? 11 :10;
-         totalDay += differ;
-         if ( totalDay %30 === 0){
-             jMonth = totalDay / 30 + 9;
-             jDay = 30 ;
-         } else {
-             jMonth = Math.floor(totalDay / 30) + 10;
-             jDay = totalDay % 30 ;
-         }
-         jYear = year -622
-     }
-     return[jYear, jMonth,jDay]
-}
 
 function convert (e){
-    let value = document.getElementById("date").value;
-    let [year, month, day] = value.split("-");
-    year *=1;
-    month *=1;
-    day *=1;
-    let jDateArr = jDate (year, month,day);
+    let date = document.getElementById("date").value;
+    let dateArr = date.split("-");
+    console.log(dateArr);
+
+    let year = dateArr[0];
+    let month = dateArr[1];
+    let day = dateArr[2];
+
+    year =  parseInt(year)
+    month =parseInt(month);
+    day =parseInt(day);
+
+    let shamsiDateArr = shamsiDate (year, month,day);
     document.getElementById("res").innerHTML =
-    jDateArr[2] +
-    " " +
-    monthNameConvertor(jDateArr[1]) +
-    "  ماه سال" + 
-    jDateArr[0];
+    shamsiDateArr[2] +" " + persianMonthName(shamsiDateArr[1]) + "  ماه " + shamsiDateArr[0];
 }
 
-function monthNameConvertor(jMonth) {
-    switch (jMonth){
+function persianMonthName(shamsiMonth) {
+    switch (shamsiMonth){
         case 1 :
             return "فروردین";
         case 2 :
